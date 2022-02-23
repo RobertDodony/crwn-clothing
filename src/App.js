137a6d27a75e758d188e38b685d363897ba58adc
RobertDodony <robertdodony@gmail.com>
2022-02-23 22:35:30 +0100

    Seperated components and added styling to them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./HomePage";
+import Homepage from "./pages/Homepage";
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Homepage />
     </div>
   );
 }
